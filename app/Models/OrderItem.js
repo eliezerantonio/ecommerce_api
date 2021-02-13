b7d42beaa016d817c503073172762c0dbbf547e2
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-02-14 00:50:56 +0100

    orderItem model

---
+++ app/Models/OrderItem.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class OrderItem extends Model {
+
+    static get traits() {
+        //dizendo que nao tem updated_at e created_at
+        return ['App/Models/Traits/NoTimestamp']
+    }
+
+    /** relacionamento com produtos
+     * 
+     */
+    products() {
+            return this.belongsTo('App/Models/Product')
+        }
+        /**
+         * Relacioanmento com Order
+         */
+    order() {
+        return this.belongsTo('App/Models/Order')
+    }
+}
+
+module.exports = OrderItem
