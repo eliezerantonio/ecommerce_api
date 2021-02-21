2dc73e4a8ba6d219a34a34ca41d8ec6dc88facfc
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-02-21 22:19:31 +0100

    register_user

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -1,11 +1,36 @@
 'use strict'
 
+const Database = use('Database')
+const User = use('App/Models/User')
+const Role = use('Role')
+
 class AuthController {
 
     async register({ request, response }) {
-        //
-    }
+            const trx = await Database.beginTransaction()
 
+            try {
+                const { name, surname, email, password } = request.all();
+
+                const user = await User.create({ name, surname, email, password }, trx);
+
+                const userRole = await Role.findBy('slug', 'client')
+
+                await user.roles().attach([userRole.id], null, trx)
+
+                await trx.commit()
+
+                return response.status(201).send({ data: user })
+            } catch (error) {
+
+                await trx.rollback()
+
+                return response.status(400).send({
+                    message: 'Erro ao realizar cadastro'
+                })
+            }
+        }
+        //Eliezer António
     async login({ request, response, auth, }) {
         //
     }
