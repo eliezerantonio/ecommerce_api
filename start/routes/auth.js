435fa2dfb6feb747915e109d44d2df6409b4ff33
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-02-21 14:44:57 +0100

    routes reset-password

--- start/routes/auth.js
+++ start/routes/auth.js
@@ -3,17 +3,28 @@
 
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
-    /** 
-     * Auth Routes
-     * 
-     */
+const Route = use('Route');
+/** 
+ * Auth Routes
+ * 
+ */
 Route.group(() => {
-        Route.post('/register', 'AuthController.register').as('auth.register')
-        Route.post('/login', 'AuthController.login').as('auth.login')
-        Route.post('/refresh', 'AuthController.refresh').as('auth.refresh')
-        Route.post('/logout', 'AuthController.logout').as('auth.logout')
+        Route.post('register', 'AuthController.register').as('auth.register');
+
+        Route.post('login', 'AuthController.login').as('auth.login');
+
+        Route.post('refresh', 'AuthController.refresh').as('auth.refresh');
+
+        Route.post('logout', 'AuthController.logout').as('auth.logout');
+
+        //restore password routes
+        Route.post('reset-password', 'AuthController.forgot').as('auth.forgot');
+
+        Route.get('reset-password', 'AuthController.remember').as('auth.remember');
+
+        Route.put('reset-password', 'AuthController.reset').as('auth.reset');
+
 
     })
     .prefix('v1/auth')
-    .namespace('Auth')
+    .namespace('Auth');
