25d4c07ab9eaf13c70ec1f204bb0d78670de04b1
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-02-21 22:28:18 +0100

    login

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -7,35 +7,40 @@ const Role = use('Role')
 class AuthController {
 
     async register({ request, response }) {
-            const trx = await Database.beginTransaction()
+        const trx = await Database.beginTransaction()
 
-            try {
-                const { name, surname, email, password } = request.all();
+        try {
+            const { name, surname, email, password } = request.all();
 
-                const user = await User.create({ name, surname, email, password }, trx);
+            const user = await User.create({ name, surname, email, password }, trx);
 
-                const userRole = await Role.findBy('slug', 'client')
+            const userRole = await Role.findBy('slug', 'client')
 
-                await user.roles().attach([userRole.id], null, trx)
+            await user.roles().attach([userRole.id], null, trx)
 
-                await trx.commit()
+            await trx.commit()
 
-                return response.status(201).send({ data: user })
-            } catch (error) {
+            return response.status(201).send({ data: user })
+        } catch (error) {
 
-                await trx.rollback()
+            await trx.rollback()
 
-                return response.status(400).send({
-                    message: 'Erro ao realizar cadastro'
-                })
-            }
+            return response.status(400).send({
+                message: 'Erro ao realizar cadastro'
+            })
         }
-        //Eliezer António
+    }
     async login({ request, response, auth, }) {
-        //
+        const { email, password } = request.all()
+        let data = await auth.withRefreshToken().attempt(email, password)
+
+        return response.send({ data })
+
     }
 
     async refresh({ request, response, auth }) {
+
+
 
         //
     }
