b7df7fa912821b8bac06018a4184be05d2720e92
eliezerantonio.com <eliezerfernandoantonio@gmail.com>
2021-03-13 19:05:17 +0100

    metodo destroy

--- app/Controllers/Http/Admin/CategoryController.js
+++ app/Controllers/Http/Admin/CategoryController.js
@@ -92,7 +92,12 @@ class CategoryController {
      * @param {Request} ctx.request
      * @param {Response} ctx.response
      */
-    async destroy({ params, request, response }) {}
+    async destroy({ params: { id }, request, response }) {
+        const category = await Category.findOrFail(id)
+        await category.delete()
+
+        return response.status(204).send({ message: "Eliminado com sucesso" })
+    }
 }
 
 module.exports = CategoryController
